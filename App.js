83d61b3f7ac22dbbb25fb338b94514836e1910f6
Marine Bianchi <marine.bianchi@gmail.com>
2023-07-31 11:29:17 +0200

    header

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Header from './components/Header';
-import * as React from 'react';
 
 export default function App() {
 
 
   return (
     <View style={styles.container}>
-<Header/>
       <StatusBar style="auto" />
     </View>
   );
