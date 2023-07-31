e48de54e5d61e8c34aef1bd65a9178ae300ff453
Wissem <wbousdira@gmail.com>
2023-07-31 17:11:03 +0200

    remise en place de la fonction connection

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import {login} from'../reducers/user'
 import {
   Image,
   KeyboardAvoidingView,
@@ -15,23 +17,59 @@ import {
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useCallback } from "react";
+
 SplashScreen.preventAutoHideAsync();
 
 export default function LoginScreen({ navigation }) {
-
+  
   const [fontsLoaded] = useFonts({
     "Montserrat": require("../assets/fonts/Montserrat-Regular.ttf"),
     "MontserratBold": require("../assets/fonts/Montserrat-Bold.ttf"),
     "Poppins": require("../assets/fonts/Poppins-Regular.ttf"),
 
   });
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
+
+  // Redirect to /home if logged in
+  // if (user.token) {
+  //   navigation.navigate('Accueil');
+  // }
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignin = () => {
+    fetch('http://10.20.2.175:3000/users/signin', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    }).then(response => response.json())
+      .then(data => {
+        if (data.result) {
+          dispatch(login({ token: data.token, email: data.email, name: data.name }));
+          navigation.navigate("TabNavigator", { screen: "Acceuil" }); // Redirect to "Accueil" page
+        } else {
+          // Handle login failure, display an error message, etc.
+          alert(data.error);
+        }
+      })
+      .catch(error => {
+        // Handle fetch error, display an error message, etc.
+        console.error('Login failed:', error);
+        alert('An error occurred while trying to log in. Please try again later.');
+      });
+  }
 
   const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
       await SplashScreen.hideAsync();
     }
   }, [fontsLoaded]);
-
+  
+  
+  
+  
   if(fontsLoaded){
     return (
 
@@ -50,30 +88,23 @@ export default function LoginScreen({ navigation }) {
             <View style={styles.form}>
               <View style={styles.email}>
                 <Text style={styles.label}>Email</Text>
-                <TextInput
-                  style={styles.input}
-                  placeholder="johndoe@gmail.com"
-                />
-              </View>
-              <View style={styles.password}>
-                <Text style={styles.label}>Mot de passe</Text>
-                <TextInput style={styles.input} placeholder="3j48wWpLkk4R9J" />
-              </View>
-              <TouchableOpacity style={styles.btnLogin}>
-                <Text style={styles.login}>Connexion</Text>
-              </TouchableOpacity>
-              <TouchableOpacity>
-                <Text>Mot de passe oublié ?</Text>
-              </TouchableOpacity>
-              <TouchableOpacity
-                onPress={() => navigation.navigate("SignUp")}
-                style={styles.signupHere}
-              >
-                <Text style={{ textAlign: "center",     fontFamily:"Poppins"
- }}>
-                  Vous n'avez pas de compte ? Inscrivez-vous ici !
-                </Text>
-              </TouchableOpacity>
+                <TextInput style={styles.input} onChangeText={(value) => setEmail(value)} value={email} placeholder="johndoe@gmail.com" />
+            </View>
+            <View style={styles.password}>
+              <Text style={styles.label}>Mot de passe</Text>
+              <TextInput style={styles.input} onChangeText={(value) => setPassword(value)} value={password} placeholder="Password" />
+            </View>
+            <TouchableOpacity onPress={() => handleSignin()}style={styles.btnLogin}>
+              <Text style={styles.login}>Connexion</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={styles.signupHere}>Mot de passe oublié ?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate("SignUp")}
+            >
+              <Text  style={styles.signupHere}>Vous n'avez pas de compte ? Inscrivez-vous ici !</Text>
+            </TouchableOpacity>
             </View>
           </View>
           <StatusBar style="auto" />
@@ -119,7 +150,8 @@ const styles = StyleSheet.create({
   },
   form: {
     width: "80%",
-    height: "70%",
+    height: 500,
+    marginTop:70,
     alignItems: "center",
   },
   input: {
