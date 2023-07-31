94d52a72c8884cf194e04b907576cc3b2037c440
mada067 <adam.kasnouar@gmail.com>
2023-07-31 14:22:45 +0200

    modif home

--- App.js
+++ App.js
@@ -57,7 +57,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="TabNavigator" component={TabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
