92c67ae5f8bffcbea1d9aef19826eef0b5b9d816
Yllies <hadjyllies@gmail.com>
2023-07-31 11:05:09 +0200

    add HomeScreen

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>ZERO</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -14,7 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
 });
---
+++ screens/HomeScreen.js
@@ -0,0 +1,121 @@
+import { useState, useEffect } from "react";
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+  StatusBar,
+  SafeAreaView,
+} from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        style={styles.mainContain}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View style={styles.topContainer}>
+          <Text style={styles.title}>
+            Bienvenue sur <Text style={styles.zero}>Zéro</Text>
+          </Text>
+        </View>
+        <View style={styles.bottomContainer}>
+          <View style={styles.form}>
+            <View style={styles.email}>
+              <Text style={styles.label}>Email</Text>
+              <TextInput style={styles.input} placeholder="johndoe@gmail.com" />
+            </View>
+            <View style={styles.password}>
+              <Text style={styles.label}>Mot de passe</Text>
+              <TextInput style={styles.input} placeholder="3j48wWpLkk4R9J" />
+            </View>
+            <TouchableOpacity style={styles.btnLogin}>
+              <Text style={styles.login}>Connexion</Text>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text>Mot de passe oublié ?</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.signupHere}>
+              <Text>Vous n'avez pas de compte ? Inscrivez-vous ici !</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <StatusBar style="auto" />
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  mainContain: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  topContainer: {
+    backgroundColor: "#274539",
+    height: "30%",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    width: "80%",
+    textAlign: "center",
+    fontSize: 60,
+  },
+  zero: {
+    color: "#EDFC92",
+  },
+  bottomContainer: {
+    height: "70%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  form: {
+    width: "80%",
+    height: "70%",
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "#F6F8F7",
+    padding: 13,
+    fontSize: 25,
+    marginTop: 10,
+    borderRadius: 4,
+    width: 300,
+  },
+  label: {
+    fontSize: 25,
+  },
+  email: {
+    marginBottom: 30,
+  },
+  password: {
+    marginBottom: 50,
+  },
+  btnLogin: {
+    backgroundColor: "#EDFC92",
+    padding: 13,
+    width: 290,
+    shadowColor: "#171717",
+    shadowOffset: { width: -2, height: 4 },
+    shadowOpacity: 0.2,
+    shadowRadius: 3,
+    alignItems: "center",
+    marginBottom: 25,
+  },
+  signupHere: {
+    marginTop: 30,
+  },
+  login: {
+    fontSize: 20,
+  },
+});
