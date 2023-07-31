40acae7ce114962baab93e6c1b83ff513df591d6
mada067 <adam.kasnouar@gmail.com>
2023-07-31 11:05:42 +0200

    tabBar

--- App.js
+++ App.js
@@ -1,12 +1,62 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import HomeScreen from "./screens/HomeScreen";
+import FavoriteScreen from "./screens/FavoriteScreen";
+import AddScreen from "./screens/AddScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  return (
+    <Tab.Navigator
+      screenOptions=
+      {({ route }) => ({
+        // style:{
+        //   margin: 50,
+        //   height: 90,
+        // },
+        tabBarIcon: ({ color, size }) => {
+          let iconName = "";
+          if (route.name === "Accueil") {
+            iconName = "home";
+          } else if (route.name === "Favoris") {
+            iconName = "heart";
+          } else if (route.name === "Ajout") {
+            iconName = "plus";
+          } else if (route.name === "Profile") {
+            iconName = "user";
+          }
+          return <FontAwesome name={iconName} size={size} color={color} />;
+        },
+
+        tabBarActiveTintColor: "#EDFC92",
+        tabBarInactiveTintColor: "#fff",
+        tabBarActiveBackgroundColor: "#274539",
+        tabBarInactiveBackgroundColor: "#274539",
+        headerShown: false,
+      })}
+    >
+      <Tab.Screen name="Accueil" component={HomeScreen} />
+      <Tab.Screen name="Favoris" component={FavoriteScreen} />
+      <Tab.Screen name="Ajout" component={AddScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>ZERO</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen style={styles.barNav} name="TabNavigator" component={TabNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -17,4 +67,5 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+
 });
