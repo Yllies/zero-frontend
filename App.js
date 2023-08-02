0db6e6891a98ea941b4a0226135d7dcd576489ca
mada067 <adam.kasnouar@gmail.com>
2023-08-02 09:09:54 +0200

    modifs style

--- App.js
+++ App.js
@@ -68,8 +68,8 @@ export default function App() {
     <Provider store={store}>
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="SignUp" component={SignUpScreen} /> */}
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} />
         <Stack.Screen name="TabNavigator" component={TabNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
