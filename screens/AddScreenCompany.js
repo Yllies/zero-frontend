2a59f4aa1f45fa909908770fc9b2bd35a7304d13
mada067 <adam.kasnouar@gmail.com>
2023-08-02 15:03:35 +0200

    ok

--- screens/AddScreenCompany.js
+++ screens/AddScreenCompany.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {
+  FlatList,
   Image,
   KeyboardAvoidingView,
   Platform,
@@ -11,10 +12,10 @@ import {
   StatusBar,
   SafeAreaView,
   ScrollView,
-  FlatList,
 } from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { AntDesign } from '@expo/vector-icons';
 
 export default function AddScreenCompany({ navigation }) {
   const [title, setTitle] = useState("");
@@ -49,6 +50,22 @@ export default function AddScreenCompany({ navigation }) {
     }
   };
 
+  // Fonction pour ouvrir l'appareil photo
+  const takePhoto = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [3, 4],
+      quality: 1,
+    });
+
+    // Vérifier si l'utilisateur a pris une photo
+    if (!result.cancelled) {
+      // Ajouter la nouvelle image prise au tableau existant
+      setSelectedImages([...selectedImages, result]);
+    }
+  };
+
   // Fonction pour supprimer une image du tableau selectedImages
   const removeImage = (imageUri) => {
     setSelectedImages(selectedImages.filter(image => image.uri !== imageUri));
@@ -58,6 +75,20 @@ export default function AddScreenCompany({ navigation }) {
   if (galleryPermission === false) {
     return <Text>Pas d'accès au stockage interne</Text>
   }
+
+  // Composant pour afficher une image sélectionnée avec l'icône de suppression
+  const SelectedImageItem = ({ item }) => (
+    <View style={styles.selectedImageItem}>
+      <Image source={{ uri: item.uri }} style={styles.selectedImage} />
+      {/* Icône "times" pour supprimer l'image */}
+      <TouchableOpacity
+        onPress={() => removeImage(item.uri)}
+        style={styles.deleteIconContainer}
+      >
+        <FontAwesome name='times' size={20} color='#EDFC92' style={styles.deleteIcon} />
+      </TouchableOpacity>
+    </View>
+  );
 
   return (
     <SafeAreaView style={styles.container}>
@@ -75,6 +106,8 @@ export default function AddScreenCompany({ navigation }) {
               <Text style={styles.label}>Titre</Text>
               <TextInput
                 style={styles.input}
+                multiline={true}
+                textAlignVertical="top"
                 onChangeText={(value) => setTitle(value)}
                 value={title}
                 placeholder="Quel est le titre de votre annonce?"
@@ -85,6 +118,8 @@ export default function AddScreenCompany({ navigation }) {
               <Text style={styles.label}>Catégorie</Text>
               <TextInput
                 style={styles.input}
+                multiline={true}
+                textAlignVertical="top"
                 onChangeText={(value) => setCategory(value)}
                 value={category}
                 placeholder="Quelle est la catégorie?"
@@ -93,41 +128,38 @@ export default function AddScreenCompany({ navigation }) {
             {/* Sélecteur d'images */}
             <View style={styles.imagePickerContainer}>
               <Text style={styles.label}>Ajouter des photos</Text>
-              <TouchableOpacity
-                onPress={() => pickImage()}
-                style={styles.imagePickerButton}
-              >
-                <Text style={styles.imagePickerButtonText}>
-                  Sélectionner des photos
-                </Text>
-              </TouchableOpacity>
-              {/* Afficher les images sélectionnées */}
-              <View style={styles.selectedImagesContainer}>
-                <FlatList
-                  data={selectedImages}
-                  horizontal
-                  showsHorizontalScrollIndicator={false}
-                  keyExtractor={(item, index) => index.toString()}
-                  renderItem={({ item }) => (
-                    <View>
-                      <Image source={{ uri: item.uri }} style={styles.selectedImage} />
-                      {/* Icône "times" pour supprimer l'image */}
-                      <TouchableOpacity
-                        onPress={() => removeImage(item.uri)}
-                        style={styles.deleteIconContainer}
-                      >
-                        <FontAwesome name='times' size={20} color='#EDFC92' style={styles.deleteIcon} />
-                      </TouchableOpacity>
-                    </View>
-                  )}
-                />
+              {/* Bouton pour ouvrir l'appareil photo */}
+              <View style={styles.cameraIconContainer}>
+                <TouchableOpacity
+                  onPress={() => pickImage()}
+                  style={styles.imagePickerButton}
+                >
+                  <Text style={styles.imagePickerButtonText}>
+                    Sélectionner dans la galerie ou
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() => takePhoto()}
+                  style={styles.cameraIcon}
+                >
+                  <AntDesign name='camera' size={20} color='#EDFC92' backgroundColor="#274539"/>
+                </TouchableOpacity>
               </View>
+              {/* Afficher les images sélectionnées */}
+              <FlatList
+                data={selectedImages}
+                renderItem={SelectedImageItem}
+                keyExtractor={(item, index) => index.toString()}
+                horizontal
+              />
             </View>
             {/* Champ pour la description */}
             <View style={styles.inputContainer}>
               <Text style={styles.label}>Description</Text>
               <TextInput
                 style={styles.input}
+                multiline={true}
+                textAlignVertical="top"
                 onChangeText={(value) => setDescription(value)}
                 value={description}
                 placeholder="Dites nous pourquoi vous n'en voulez plus"
@@ -138,6 +170,8 @@ export default function AddScreenCompany({ navigation }) {
               <Text style={styles.label}>Disponibilité</Text>
               <TextInput
                 style={styles.input}
+                multiline={true}
+                textAlignVertical="top"
                 onChangeText={(value) => setAvailability(value)}
                 value={availability}
                 placeholder="A partir de quand?"
@@ -197,23 +231,33 @@ const styles = StyleSheet.create({
     fontFamily: "Poppins",
   },
   imagePickerContainer: {
+    flex:1,
     marginBottom: 20,
+  },
+  cameraIconContainer: {
+    flex:1,
+    backgroundColor: "#F6F8F7",
+    borderRadius: 4,
+    padding: 5,
+    flexDirection: 'row',
+    alignItems: 'center'
   },
   imagePickerButton: {
     backgroundColor: "#F6F8F7",
     padding: 13,
     borderRadius: 4,
-    width: "100%",
-    marginTop: 30,
+  
     justifyContent: "center",
   },
   imagePickerButtonText: {
     fontFamily: "Poppins",
     color: "#555",
   },
+  selectedImageItem: {
+    marginRight: 10,
+  },
   selectedImage: {
     flexWrap: 'wrap',
-    marginTop: 10,
     width: 100,
     height: 100,
     resizeMode: "cover",
@@ -234,20 +278,12 @@ const styles = StyleSheet.create({
   zero: {
     color: "#EDFC92",
   },
-  selectedImagesContainer: {
-    flex: 1,
-  },
   deleteIconContainer: {
     position: 'absolute',
-    top: 12,
-    right: 9,
-    zIndex: 1,
+    top: 6,
+    right: 6,
     backgroundColor: 'rgba(39, 69, 57, 0.7)',
-    // padding: 5,
     borderRadius: 100,
-  },
-  deleteIcon: {
-margin:5,
-
+    padding:2,
   },
 });
