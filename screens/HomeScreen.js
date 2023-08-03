b003beb082243a8b08b492097f3b6a0ccaa50878
mada067 <adam.kasnouar@gmail.com>
2023-08-03 17:04:27 +0200

    odifs

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   View,
@@ -6,6 +6,7 @@ import {
   Text,
   TouchableOpacity,
   ScrollView,
+  ActivityIndicator,
 } from "react-native";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import Header from "../components/Header";
@@ -13,13 +14,33 @@ import ArticleDetails from "../components/ArticleDetails";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
+const BACK_URL = process.env.EXPO_PUBLIC_BACK_URL;
 
 export default function HomeScreen({ navigation }) {
-  const articlesCards = [];
+  const [posts, setPosts] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  for (let i = 0; i < 10; i++) {
-    articlesCards.push(<ArticleDetails key={i} />);
-  }
+  useEffect(() => {
+    // Appeler la fonction pour récupérer les posts depuis le backend ou une API REST
+    fetchPosts();
+  }, []);
+
+  // Fonction pour récupérer les posts depuis le backend ou une API REST
+  const fetchPosts = () => {
+    fetch(`${BACK_URL}:3000/posts/company`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.posts) {
+          setPosts(data.posts);
+        } else {
+          setError("Erreur inconnue !");
+        }
+      })
+      .catch((error) => {
+        setError("Erreur lors de la récupération des posts :" + error.message);
+      });
+  };
 
   return (
     <ScrollView>
@@ -35,7 +56,16 @@ export default function HomeScreen({ navigation }) {
           />
         </View>
 
-        <View style={styles.cardsRow}>{articlesCards}</View>
+        <View style={styles.cardsRow}>
+          {posts.map((post, index) => (
+            <ArticleDetails style={styles.titre}
+              key={index}
+              title={post.title}
+              description={post.description}
+              photo={post.photo[0]}
+            />
+          ))}
+        </View>
       </View>
     </ScrollView>
   );
@@ -64,6 +94,7 @@ const styles = StyleSheet.create({
     // paddingHorizontal: 20,
     width: "100%",
     alignItems: "center",
+    fontSize:10,
   },
 
   cardContainer: {
@@ -78,13 +109,13 @@ const styles = StyleSheet.create({
   },
 
   titre: {
-    fontSize: 17,
+    fontSize: 10,
     color: "white",
-    marginBottom: 5,
+    // marginBottom: 5,
   },
 
   paragraphe: {
-    fontSize: 12,
+    fontSize: 10,
     fontFamily: "Poppins",
     color: "white",
     marginBottom: -15,
